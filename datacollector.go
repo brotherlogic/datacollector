a46078ab69c56bc9d2021ddd4eb00315dd6372d7
Simon Tucker <simon@brotherlogic@gmail>
2018-10-31 07:28:07 -0700

    Adds a collect phase.

--- datacollector.go
+++ datacollector.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
 
 	"github.com/brotherlogic/goserver"
+	"github.com/brotherlogic/goserver/utils"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
@@ -14,10 +16,35 @@ import (
 	pbg "github.com/brotherlogic/goserver/proto"
 )
 
+type retriever interface {
+	retrieve(ctx context.Context, server, job string) (*pbg.ServerState, error)
+}
+
+type prodRetriever struct{}
+
+func (p prodRetriever) retrieve(ctx context.Context, server, job string) (*pbg.ServerState, error) {
+	servers, err := utils.ResolveAll(job)
+	if err == nil {
+		for _, s := range servers {
+			if s.Identifier == server {
+				conn, err := grpc.Dial(s.Ip+":"+strconv.Itoa(int(s.Port)), grpc.WithInsecure())
+				if err == nil {
+					defer conn.Close()
+					client := pbg.NewGoserverServiceClient(conn)
+					return client.State(ctx, &pbg.Empty{})
+				}
+			}
+		}
+	}
+
+	return &pbg.ServerState{}, fmt.Errorf("Unable to locate %v on %v", job, server)
+}
+
 //Server main server type
 type Server struct {
 	*goserver.GoServer
-	config *pb.Config
+	config    *pb.Config
+	retriever retriever
 }
 
 // Init builds the server
@@ -25,6 +52,7 @@ func Init() *Server {
 	s := &Server{
 		&goserver.GoServer{},
 		&pb.Config{},
+		prodRetriever{},
 	}
 	return s
 }
